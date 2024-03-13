70f93e41498aae92de412bbd12fe01e41900be65
Yash <yashnegi36442@gmail.com>
2024-03-13 20:22:33 +0530

    third

---
+++ SDOT/EvaluatePostfix.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Main
+{
+  
+  public static int evaluate(String str){
+    Stack<Integer> stack = new Stack<>();
+    for (int i=0; i<str.length(); i++){
+      char ch = str.charAt(i);
+      if (Character.isDigit(ch)){
+        stack.push(ch-'0');
+      }
+      else{
+        int val2 = stack.pop();
+        int val1 = stack.pop();
+        
+        switch (ch){
+          case '+':
+            stack.push(val1+val2);
+            break;
+          case '-':
+            stack.push(val1-val2);
+            break;
+          case '*':
+            stack.push(val1*val2);
+            break;
+          case '/':
+            stack.push(val1/val2);
+            break;
+        }
+      }
+    }
+    return stack.pop();
+  }
+  
+  
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int testcases = sc.nextInt();
+    sc.nextLine();
+    while (testcases > 0){
+      testcases--;
+      String str = sc.nextLine();
+      int result = evaluate(str);
+      System.out.println(result);
+    }
+  }
+}
